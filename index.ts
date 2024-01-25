8f198d71b3a29480cebcec2a1ad0709fda875b9c
Alexander Chertkov <yampi.ru@gmail.com>
2024-01-25 12:58:56 +0000

    init

---
+++ index.ts
@@ -0,0 +1,27 @@
+// 1 - в каком порядке отобразятся цифры?
+setTimeout(() => {
+  console.log(1)
+  new Promise((res, rej) => {
+      rej()
+      console.log(2)
+  }).catch(() => {
+      console.log(3)
+  })
+  console.log(4)
+})
+setTimeout(() => {
+  console.log(5)
+})
+console.log(6)
+// введи цифры в порядке отображения в консоли:
+// 
+
+// 2 - запусти этот код
+
+// 3 - реализуй функцию deepClone(obj)
+// function deepClone<T = unknown>(obj: T): T {
+//   // TODO implement
+//   return obj;
+// }
+// console.log(deepClone({ a: 1 }))
+// console.log(deepClone({ a: 1, b: [{ c: 1, g: Symbol(2) }] }))
